ff3cbb65c0b7e4845ece6c41a6fc7e6c5b44ed58
Vladimir <vv1111vv@yandex.ru>
2022-10-16 01:47:56 +0300

    Проект Shareit

---
+++ src/main/java/ru/practicum/shareit/ShareitApplication.java
@@ -0,0 +1,13 @@
+package ru.practicum.shareit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShareitApplication.class, args);
+    }
+
+}
